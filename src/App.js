657b90ad73caf8a6c8fbcadcefb012c4d0256c67
mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-04-24 20:15:34 +0200

    created the cart component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import {useState, useEffect} from "react";
 
 import {commerce} from "./lib/commerce";
-import {Products, NavBar} from "./components";
+import {Products, NavBar, Cart} from "./components";
 
 function App() {
 	const [products, setProducts] = useState([]);
 	const [cart, setCart] = useState([]);
+	// const [isLoading, setIsLoading] = useState(true);
 
 	const fetchProducts = async () => {
+		// setIsLoading(true);
 		const {data} = await commerce.products.list();
 
 		setProducts(data);
+		// setIsLoading(false);
 	};
 
 	const fetchCart = async () => {
+		// setIsLoading(true);
 		const cart = await commerce.cart.retrieve();
 
 		setCart(cart);
+		// setIsLoading(false);
 	};
 
 	const handleAddToCart = async (productID, quantity) => {
@@ -30,14 +35,15 @@ function App() {
 		fetchCart();
 	}, []);
 
-	console.log(cart);
-
 	return (
-		<div>
+		// !isLoading && (
+		<>
 			<NavBar totalItems={cart.total_items} />
-			<Products products={products} onAddToCart={handleAddToCart} />
-		</div>
+			{/* <Products products={products} onAddToCart={handleAddToCart} /> */}
+			<Cart cart={cart} />
+		</>
 	);
+	// );
 }
 
 export default App;
