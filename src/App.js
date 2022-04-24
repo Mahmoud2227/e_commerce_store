6f74ff3490e695b7b75b3494d3590bae5e3e5bc3
mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-04-24 14:20:13 +0200

    created products and navbar components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+// import NavBar from "./components/NavBar/NavBar";
+// import Products from "./components/Products/Products";
+
+import {Products, NavBar} from "./components";
+
+function App() {
+	return (
+		<div>
+			<NavBar />
+			<Products />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+	AppBar,
+	Toolbar,
+	IconButton,
+	Badge,
+	MenuItem,
+	MenuItemTypeMap,
+	Typography,
+} from "@material-ui/core";
+import {ShoppingCart} from "@material-ui/icons";
+
+import logo from "../../assets/ecommerce.png";
+import useStyles from "./NavBarStyles";
+
+const NavBar = () => {
+	const classes = useStyles();
+
+	return (
+		<>
+			<AppBar position='fixed' className={classes.appBar} color='inherit'>
+				<Toolbar>
+					<Typography variant='h6' className={classes.badge}>
+						<img src={logo} alt='ECommerce' height='25px' className={classes.image} />
+						ECommerce
+					</Typography>
+					<div className={classes.grow} />
+					<div className={classes.button}>
+						<IconButton aria-label='Show cart items' color='inherit'>
+							<Badge badgeContent={4} color='secondary' overlap='rectangular'>
+								<ShoppingCart />
+							</Badge>
+						</IconButton>
+					</div>
+				</Toolbar>
+			</AppBar>
+		</>
+	);
+};
+
+export default NavBar;
